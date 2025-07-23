184a856f9dc48d56aae49952fe38bce136ab4c95
machageDev <machagefranklyn@gmail.com>
2025-07-23 02:15:36 -1200

    awoh

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from core.models import FoodItem
+
 # Create your views here.
+
+
+def home(request):
+    items = FoodItem.objects.filter(available=True)
+    return render(request, 'home.html', {'items': items})
