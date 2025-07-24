26f03f3565109fe2fbda08b87903b5b931324351
machageDev <machagefranklyn@gmail.com>
2025-07-24 01:43:45 -1200

    class Car

--- car/models.py
+++ car/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='cars/')  # make sure MEDIA settings are configured
+
+    def __str__(self):
+        return self.name
