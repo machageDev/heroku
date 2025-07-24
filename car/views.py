911f02595458a9a14084f034983b49e48a939508
machageDev <machagefranklyn@gmail.com>
2025-07-24 01:45:03 -1200

    views.py render

--- car/views.py
+++ car/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import Car
+
+def home(request):
+    cars = Car.objects.all()
+    return render(request, 'home.html', {'cars': cars})
+
+def add_to_cart(request, car_id):
+    cart = request.session.get('cart', [])
+    if car_id not in cart:
+        cart.append(car_id)
+    request.session['cart'] = cart
+    return redirect('home')
